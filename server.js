bd32987e01bcd8988d91562d57d7a8714d382451
shubhamjatania17 <shubham.jatania@gmail.com>
2026-04-12 21:51:47 +0530

    Tackling spin down issue

--- server.js
+++ server.js
@@ -15,6 +15,22 @@ const CORS_HEADERS = {
   'Access-Control-Allow-Methods': 'GET,POST,OPTIONS'
 };
 
+const url = `https://medresponse.onrender.com/`; // Replace with your Render URL
+const interval = 30000; // Interval in milliseconds (30 seconds)
+
+//Reloader Function
+function reloadWebsite() {
+  axios.get(url)
+    .then(response => {
+      console.log(`Reloaded at ${new Date().toISOString()}: Status Code ${response.status}`);
+    })
+    .catch(error => {
+      console.error(`Error reloading at ${new Date().toISOString()}:`, error.message);
+    });
+}
+
+setInterval(reloadWebsite, interval);
+
 const htmlCache = {
   content: '',
   etag: '',
